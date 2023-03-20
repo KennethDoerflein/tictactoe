907bed8f369603eb3d45a9886f0344a1e69a61a8
Kenneth Doerflein <98554038+KennethDoerflein@users.noreply.github.com>
2023-03-20 01:12:31 -0400

    Add files via upload

---
+++ TicTacToe2.java
@@ -0,0 +1,213 @@
+// GUI Source https://copyassignment.com/gui-tic-tac-toe-game-in-java/
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class TicTacToe2 extends JPanel implements ActionListener {
+
+    // core logic variables
+    boolean playerX; // true if player X's turn, false if player O's turn
+    boolean gameDone = false; // true if game is over
+    int winner = -1; // 0 if X wins, 1 if O wins, -1 if no winner yet
+    int player1wins = 0, player2wins = 0; // number of wins for each player
+    int[][] board = new int[3][3]; // 0 if empty, 1 if X, 2 if O
+
+    // paint variables
+    int lineWidth = 5; // width of the lines
+    int lineLength = 270; // length of the lines
+    int x = 15, y = 100; // location of first line
+    int offset = 95; // square width
+    int a = 0; // used for drawing the X's and O's
+    int b = 5; // used for drawing the X's and O's
+    int selX = 0; // selected square x
+    int selY = 0; // selected square y
+
+    // COLORS
+    Color  white = new Color(255,255,255);
+    Color  black = new Color(0,0,0);
+
+    // CONSTRUCTOR
+    public TicTacToe2() {
+        Dimension size = new Dimension(450, 300); // size of the panel
+        setPreferredSize(size);
+        setMaximumSize(size);
+        setMinimumSize(size);
+        addMouseListener(new XOListener()); // add mouse listener
+    }
+
+    public void paintComponent(Graphics page) {
+        super.paintComponent(page);
+        drawBoard(page);
+        drawUI(page);
+        drawGame(page);
+    }
+
+    public void drawBoard(Graphics page) {
+        setBackground( white);
+        page.setColor( black);
+        page.fillRoundRect(x, y, lineLength, lineWidth, 5, 30);
+        page.fillRoundRect(x, y + offset, lineLength, lineWidth, 5, 30);
+        page.fillRoundRect(y, x, lineWidth, lineLength, 30, 5);
+        page.fillRoundRect(y + offset, x, lineWidth, lineLength, 30, 5);
+    }
+
+    public void drawUI(Graphics page) {
+        // SET COLOR AND FONT
+        page.setColor( white);
+        page.fillRect(300, 0, 120, 300);
+        Font font = new Font("Helvetica",  Font.BOLD, 20);
+        page.setFont(font);
+
+        // SET WIN COUNTER
+        page.setColor( black);
+        page.drawString("Win Count", 310, 30);
+        page.drawString(": " + player1wins, 362, 70);
+        page.drawString(": " + player2wins, 362, 105);
+
+        // DRAW score X
+        ImageIcon xIcon = new ImageIcon("blackX.png");
+        Image xImg = xIcon.getImage();
+        Image newXImg = xImg.getScaledInstance(27, 27, Image.SCALE_SMOOTH);
+        ImageIcon newXIcon = new ImageIcon(newXImg);
+        page.drawImage(newXIcon.getImage(), 44 + offset * 1 + 190, 47 + offset * 0, null);
+
+        // DRAW score O
+        page.setColor( black);
+        page.fillOval(43 + 190 + offset, 80, 30, 30);
+        page.setColor( white);
+        page.fillOval(49 + 190 + offset, 85, 19, 19);
+
+        // DRAW WHO'S TURN or WINNER
+        page.setColor( black);
+        Font font1 = new Font(" Courier",  Font.BOLD, 18);
+        page.setFont(font1);
+
+        if (gameDone) {
+            if (winner == 1) { // x
+                page.drawString("The winner is", 310, 150);
+                page.drawImage(xImg, 335, 160, null);
+            } else if (winner == 2) { // o
+                page.drawString("The winner is", 310, 150);
+                page.setColor( black);
+                page.fillOval(332, 160, 50, 50);
+                page.setColor( white);
+                page.fillOval(342, 170, 30, 30);
+            } else if (winner == 3) { // tie
+                page.drawString("It's a tie", 330, 178);
+            }
+        } else {
+            Font font2 = new Font(" Courier",  Font.BOLD, 24);
+            page.setFont(font2);
+            page.drawString("", 350, 160);
+            if (playerX) {
+                page.drawString("X 's Turn", 315, 180);
+            } else {
+                page.drawString("O 's Turn", 315, 180);
+            }
+        }
+        Font c = new Font("Courier", Font.BOLD + Font.CENTER_BASELINE, 13);
+        page.setFont(c);
+    }
+
+    public void drawGame(Graphics page) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == 0) {
+
+                } else if (board[i][j] == 1) {
+                    ImageIcon xIcon = new ImageIcon("blackX.png");
+                    Image xImg = xIcon.getImage();
+                    page.drawImage(xImg, 30 + offset * i, 30 + offset * j, null);
+                } else if (board[i][j] == 2) {
+                    page.setColor( black);
+                    page.fillOval(30 + offset * i, 30 + offset * j, 50, 50);
+                    page.setColor( white);
+                    page.fillOval(40 + offset * i, 40 + offset * j, 30, 30);
+                }
+            }
+        }
+        repaint();
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Tic Tac Toe");
+        frame.getContentPane();
+
+        TicTacToe2 gamePanel = new TicTacToe2();
+        frame.add(gamePanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private class XOListener implements MouseListener {
+
+        public void mouseClicked(MouseEvent event) {
+            selX = -1;
+            selY = -1;
+            if (gameDone == false) {
+                a = event.getX();
+                b = event.getY();
+                int selX = 0, selY = 0;
+                if (a > 12 && a < 99) {
+                    selX = 0;
+                } else if (a > 103 && a < 195) {
+                    selX = 1;
+                } else if (a > 200 && a < 287) {
+                    selX = 2;
+                } else {
+                    selX = -1;
+                }
+
+                if (b > 12 && b < 99) {
+                    selY = 0;
+                } else if (b > 103 && b < 195) {
+                    selY = 1;
+                } else if (b > 200 && b < 287) {
+                    selY = 2;
+                } else {
+                    selY = -1;
+                }
+                if (selX != -1 && selY != -1) {
+
+                    if (board[selX][selY] == 0) {
+                        if (playerX) {
+                            board[selX][selY] = 1;
+                            playerX = false;
+                        } else {
+                            board[selX][selY] = 2;
+                            playerX = true;
+                        }
+                        //checkWinner();
+                        //System.out.println(" CLICK= x:" + a + ",y: " + b + "; selX,selY: " + selX + "," + selY);
+
+                    }
+                } else {
+                    //System.out.println("invalid click");
+                }
+            }
+        }
+
+        public void mouseReleased(MouseEvent event) {
+        }
+
+        public void mouseEntered(MouseEvent event) {
+        }
+
+        public void mouseExited(MouseEvent event) {
+        }
+
+        public void mousePressed(MouseEvent event) {
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
+
+}
